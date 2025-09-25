1f347d4ddcd4a1f6d687a430a2790194e22bbd4a
fraiori0 <fraiori@hotmail.it>
2025-09-25 09:37:38 +0000

    --

--- setup.py
+++ setup.py
@@ -9,8 +9,7 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
@@ -21,7 +20,6 @@ setup(
     maintainer_email='f.iori@santannapisa.it',
     description='Rosbag recording manager for ROS2',
     license='TODO: License declaration',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'recording_node = ros2_bag_recorder.recording_node:main',
